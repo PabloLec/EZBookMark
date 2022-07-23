1ce51d02b94d048d488c069307d72c5c8842fdea
PabloLec <pablo.lecolinet@gmail.com>
2022-07-23 13:36:12 +0200

    Add RecyclerView custom listener

--- app/src/main/java/dev/pablolec/ezboookmark/MainActivity.java
+++ app/src/main/java/dev/pablolec/ezboookmark/MainActivity.java
@@ -5,26 +5,30 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.List;
 
 import dev.pablolec.ezboookmark.adapter.MainListAdapter;
 import dev.pablolec.ezboookmark.dao.BookmarkDao;
+import dev.pablolec.ezboookmark.listener.RecyclerTouchListener;
 import dev.pablolec.ezboookmark.model.Bookmark;
 
 public class MainActivity extends AppCompatActivity {
-    private RecyclerView mMainListView;
+    private RecyclerView mMainRecyclerView;
     private MainListAdapter mMainListAdapter;
+    private List<Bookmark> bookmarkList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mMainListView = findViewById(R.id.main_recycler_view);
-        mMainListView.setLayoutManager(new LinearLayoutManager(this));
+        mMainRecyclerView = findViewById(R.id.main_recycler_view);
+        mMainRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         try {
             loadBookmarks();
         } catch (Exception e) {
@@ -32,10 +36,20 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
     private void loadBookmarks() throws Exception {
-        List<Bookmark> bookmarkList = new BookmarkDao().loadAll();
+        bookmarkList = new BookmarkDao().loadAll();
         mMainListAdapter = new MainListAdapter(bookmarkList);
-        mMainListView.setAdapter(mMainListAdapter);
+        mMainRecyclerView.setAdapter(mMainListAdapter);
+        mMainRecyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), mMainRecyclerView, new RecyclerTouchListener.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Toast.makeText(getApplication(), bookmarkList.get(position).getUrl(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
     }
 }
