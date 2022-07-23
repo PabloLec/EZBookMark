1283e84918eadc21c92874cb356dd072f98cbd37
PabloLec <pablo.lecolinet@gmail.com>
2022-07-23 11:55:15 +0200

    Add test database

--- app/src/main/java/dev/pablolec/ezboookmark/dao/BookmarkDao.java
+++ app/src/main/java/dev/pablolec/ezboookmark/dao/BookmarkDao.java
@@ -1,5 +1,27 @@
 package dev.pablolec.ezboookmark.dao;
 
-public class BookmarkDao {
+import com.google.gson.GsonBuilder;
 
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+
+import dev.pablolec.ezboookmark.App;
+import dev.pablolec.ezboookmark.model.Bookmark;
+
+public class BookmarkDao {
+    private final String testDataBaseFile = "database.json";
+
+    public List<Bookmark> loadAll() throws Exception {
+        InputStream is = App.getContext().getAssets().open(testDataBaseFile);
+        int size = is.available();
+        byte[] buffer = new byte[size];
+        is.read(buffer);
+        is.close();
+        String jsonString = new String(buffer, StandardCharsets.UTF_8);
+
+        Bookmark[] bookMarkArray = new GsonBuilder().create().fromJson(jsonString, Bookmark[].class);
+        return Arrays.asList(bookMarkArray);
+    }
 }
