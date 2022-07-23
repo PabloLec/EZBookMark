2dc7090dcf65cae8461c36c435f25c0da983cae3
PabloLec <pablo.lecolinet@gmail.com>
2022-07-23 13:17:33 +0200

    Working list

--- app/src/main/java/dev/pablolec/ezboookmark/MainActivity.java
+++ app/src/main/java/dev/pablolec/ezboookmark/MainActivity.java
@@ -1,20 +1,41 @@
 package dev.pablolec.ezboookmark;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import java.util.List;
+
 import dev.pablolec.ezboookmark.adapter.MainListAdapter;
+import dev.pablolec.ezboookmark.dao.BookmarkDao;
+import dev.pablolec.ezboookmark.model.Bookmark;
 
 public class MainActivity extends AppCompatActivity {
-    private ListView mMainListView;
+    private RecyclerView mMainListView;
+    private MainListAdapter mMainListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mMainListView = findViewById(R.id.main_list_view);
-        mMainListView.setAdapter(new MainListAdapter());
+        mMainListView = findViewById(R.id.main_recycler_view);
+        mMainListView.setLayoutManager(new LinearLayoutManager(this));
+        try {
+            loadBookmarks();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void loadBookmarks() throws Exception {
+        List<Bookmark> bookmarkList = new BookmarkDao().loadAll();
+        mMainListAdapter = new MainListAdapter(bookmarkList);
+        mMainListView.setAdapter(mMainListAdapter);
     }
 }
--- app/src/main/java/dev/pablolec/ezboookmark/adapter/MainListAdapter.java
+++ app/src/main/java/dev/pablolec/ezboookmark/adapter/MainListAdapter.java
@@ -1,68 +1,73 @@
 package dev.pablolec.ezboookmark.adapter;
 
+import android.content.Context;
 import android.database.DataSetObserver;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainListAdapter implements ListAdapter {
-    @Override
-    public boolean areAllItemsEnabled() {
-        return false;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import dev.pablolec.ezboookmark.R;
+import dev.pablolec.ezboookmark.model.Bookmark;
+
+public class MainListAdapter extends RecyclerView.Adapter<MainListAdapter.ViewHolder> {
+
+    private List<Bookmark> bookmarkList;
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView textView;
+
+        public ViewHolder(View view) {
+            super(view);
+            // Define click listener for the ViewHolder's View
+
+            textView = (TextView) view.findViewById(R.id.url_text_view);
+        }
+
+        public TextView getTextView() {
+            return textView;
+        }
     }
 
-    @Override
-    public boolean isEnabled(int i) {
-        return false;
+    public MainListAdapter(List<Bookmark> bookmarkList) {
+        this.bookmarkList = bookmarkList;
     }
 
+    // Create new views (invoked by the layout manager)
+    @NonNull
     @Override
-    public void registerDataSetObserver(DataSetObserver dataSetObserver) {
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new view, which defines the UI of the list item
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.bookmark_cell, viewGroup, false);
 
+        return new ViewHolder(view);
     }
 
+    // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
 
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.getTextView().setText(bookmarkList.get(position).getUrl());
     }
 
+    // Return the size of your dataset (invoked by the layout manager)
     @Override
-    public int getCount() {
-        return 0;
+    public int getItemCount() {
+        return bookmarkList.size();
     }
 
-    @Override
-    public Object getItem(int i) {
-        return null;
-    }
-
-    @Override
-    public long getItemId(int i) {
-        return 0;
-    }
-
-    @Override
-    public boolean hasStableIds() {
-        return false;
-    }
-
-    @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
-    }
-
-    @Override
-    public int getItemViewType(int i) {
-        return 0;
-    }
-
-    @Override
-    public int getViewTypeCount() {
-        return 0;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
 }
