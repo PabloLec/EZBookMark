6ac0848b5c437c3e6b897bd5fcbe7dc2c35be151
PabloLec <pablo.lecolinet@gmail.com>
2022-07-27 17:42:51 +0200

    Rename lists fragment

--- app/src/main/java/dev/pablolec/ezbookmark/ui/gallery/ListsFragment.java
+++ app/src/main/java/dev/pablolec/ezbookmark/ui/gallery/ListsFragment.java
@@ -1,4 +1,4 @@
-package dev.pablolec.ezbookmark.ui.gallery;
+package dev.pablolec.ezbookmark.ui.lists;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import dev.pablolec.ezbookmark.databinding.FragmentGalleryBinding;
+import dev.pablolec.ezbookmark.databinding.FragmentListsBinding;
 
-public class GalleryFragment extends Fragment {
+public class ListsFragment extends Fragment {
 
-    private FragmentGalleryBinding binding;
+    private FragmentListsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        GalleryViewModel galleryViewModel =
-                new ViewModelProvider(this).get(GalleryViewModel.class);
+        ListsModel listsModel =
+                new ViewModelProvider(this).get(ListsModel.class);
 
-        binding = FragmentGalleryBinding.inflate(inflater, container, false);
+        binding = FragmentListsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textLists;
+        listsModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- app/src/main/java/dev/pablolec/ezbookmark/ui/gallery/ListsModel.java
+++ app/src/main/java/dev/pablolec/ezbookmark/ui/gallery/ListsModel.java
@@ -1,16 +1,16 @@
-package dev.pablolec.ezbookmark.ui.gallery;
+package dev.pablolec.ezbookmark.ui.lists;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class ListsModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public ListsModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        mText.setValue("This is lists fragment");
     }
 
     public LiveData<String> getText() {
